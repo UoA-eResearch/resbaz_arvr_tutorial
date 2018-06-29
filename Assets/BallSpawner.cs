0e872acf3e73f91af8be8f83df8b06ab97a12482
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-06-29 13:47:02 +1200

    init

---
+++ Assets/BallSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class BallSpawner : MonoBehaviour {
+
+	GestureRecognizer recognizer;
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		recognizer = new GestureRecognizer();
+		recognizer.Tapped += Recognizer_Tapped;
+		recognizer.StartCapturingGestures();
+	}
+
+	private void Recognizer_Tapped(TappedEventArgs obj)
+	{
+		Debug.Log("tap");
+		var ball = Instantiate(ballPrefab);
+		ball.GetComponent<Renderer>().material.color = Random.ColorHSV();
+		ball.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+		ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * 1000);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
